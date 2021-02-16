af14fe3ae0ebcd47d9e4b951c2fab7595dacbcef
Hannah Malm <hannahemalm616@gmail.com>
2021-02-16 02:02:54 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,5 @@
 class Artist 
-  o	Attr_accessor :name, :genre
-o	Def initialize(name, genre)
-	@name = name
-	@genre = genre
-o	End 
+  attr_accessor :name
+  def initialize(name)
+  @name = name
+end 
